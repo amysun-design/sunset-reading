df07c6505953b110a8486a8f68a75558ea168e69
amysun-design <contact@amysun.design>
2025-05-28 19:43:19 -0400

    Commit

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// need help with the following: email address validation, selecting logo takes you to default/home page, "Reviews" changes middle content without switching to another html, and selecting hamburger menu opens dropdown menu
+// need help with the following: selecting logo takes you to default/home page, "Reviews" changes middle content without switching to another html
 
-const hamburgerIcon = document.querySelector("#hamburger-icon")
+const hamburgerIcon = document.querySelector(".hamburger-icon")
 const dropdownMenu = document.querySelector(".dropdown-menu")
 
-hamburgerIcon.addEventListener("click", () => {
-    if (dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none";
-    }
-    else {dropdownMenu.style.display ="block"}
+hamburgerIcon.addEventListener("click", (e) => {
+    dropdownMenu.classList.toggle("show")
+})
+
+const cardsContent = document.querySelector(".cards")
+const reviewsButton = document.querySelector("#reviewsButton")
+const newContent = document.querySelector("#newContent")
+
+reviewsButton.addEventListener("click", (e) => {
+    cardsContent.style.display = "none"
+    newContent.style.display = "block"
+})
+
+const logoImage = document.querySelector("#logo-image")
+
+logoImage.addEventListener("click", (e) => {
+    cardsContent.style.display = "block"
+    newContent.style.display = "none"
 })
