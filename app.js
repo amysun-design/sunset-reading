88c2024e4a12a0852d6f7720f09808ff28b7ca45
amysun-design <contact@amysun.design>
2025-05-28 20:15:29 -0400

    Commit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// need help with the following: selecting logo takes you to default/home page, "Reviews" changes middle content without switching to another html
-
 const hamburgerIcon = document.querySelector(".hamburger-icon")
 const dropdownMenu = document.querySelector(".dropdown-menu")
 
 hamburgerIcon.addEventListener("click", (e) => {
     dropdownMenu.classList.toggle("show")
 })
+
 
 const cardsContent = document.querySelector(".cards")
 const reviewsButton = document.querySelector("#reviewsButton")
@@ -15,6 +14,7 @@ reviewsButton.addEventListener("click", (e) => {
     cardsContent.style.display = "none"
     newContent.style.display = "block"
 })
+
 
 const logoImage = document.querySelector("#logo-image")
 
